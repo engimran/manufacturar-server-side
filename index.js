feb0c61486fed38c1b33b0ea4632e1382ff9fe9c
Imran Hossen <imranhossen20@gmail.com>
2022-05-27 02:02:59 +0600

    data recieved from client side

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = "mongodb+srv://technology:G1bLQrOu15eCGUip@cluster0.xy3vc.mongodb.net/?retryWrites=true&w=majority";
-console.log(uri)
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -17,12 +17,28 @@ async function run() {
     try {
         await client.connect();
         const productsCollection = client.db("technology_holdings").collection("products");
+        const orderCollection = client.db("technology_holdings").collection("order");
 
+        // products shown
         app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productsCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
+        });
+
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const order = await orderCollection.find(query).toArray();
+            res.send(order);
+        })
+
+        // order place
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         })
     }
     finally {
